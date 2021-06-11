6b429d9fe301727aad1e7ca874b49a1c2c038a3c
Germano <germanofortunato@hotmail.it>
2021-06-11 12:46:23 +0200

    Ho implementato il ControllerAuto. closes #1

---
+++ src/main/java/it/ntt/concessionaria/controllers/ControllerAuto.java
@@ -0,0 +1,24 @@
+package it.ntt.concessionaria.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import it.ntt.concessionaria.repositories.AutoRepository;
+
+@Controller
+public class ControllerAuto {
+
+    private final AutoRepository autoRepository;
+
+    public ControllerAuto(AutoRepository autoRepository) {
+        this.autoRepository = autoRepository;
+    }
+
+    @GetMapping("/automobili")
+    public String getAuto(Model model) {
+        model.addAttribute("automobili", autoRepository.findAll());
+        return "auto";
+    }
+    
+}
