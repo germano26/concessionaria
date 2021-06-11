0e1c05ee11e6edaa0c0627d921b5510f3337084a
Germano <germanofortunato@hotmail.it>
2021-06-11 15:39:01 +0200

    Ho aggiunto la pagina listaconcessionari.

---
+++ src/main/java/it/ntt/concessionaria/controllers/ControllerConcessionari.java
@@ -0,0 +1,30 @@
+package it.ntt.concessionaria.controllers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// import antlr.collections.List;
+// import it.ntt.concessionaria.models.Concessionario;
+import it.ntt.concessionaria.repositories.ConcessionarioRepository;
+
+@Controller
+public class ControllerConcessionari {
+    
+    private ConcessionarioRepository concessionarioRepository;
+
+    public ControllerConcessionari (ConcessionarioRepository concessionarioRepository) {
+        this.concessionarioRepository = concessionarioRepository;
+    }
+
+    @GetMapping("/listaconcessionari")
+    public String getAllConcessionari(Model model) {
+        // Set<Concessionario> listaConcessionari = new HashSet<>();
+        // listaConcessionari.addAll(concessionarioRepository.findAll());
+        model.addAttribute("listaConcessionari", concessionarioRepository.findAll());
+        return "listaconcessionari";
+    }
+}
