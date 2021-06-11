9b473da22103d6061879c038474cedddbc2fa1b1
Germano <germanofortunato@hotmail.it>
2021-06-11 11:29:25 +0200

    first commit

---
+++ src/test/java/it/ntt/concessionaria/ConcessionariaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ntt.concessionaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
