fd5fd6bf72ee12ace0eda09a4bc368f431ffed0e
Germano <germanofortunato@hotmail.it>
2021-06-14 10:40:40 +0200

    closes #2, #3

--- src/main/java/it/ntt/concessionaria/controllers/ControllerAuto.java
+++ src/main/java/it/ntt/concessionaria/controllers/ControllerAuto.java
@@ -10,18 +10,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.ntt.concessionaria.models.Auto;
 import it.ntt.concessionaria.models.Concessionario;
 import it.ntt.concessionaria.repositories.AutoRepository;
+import it.ntt.concessionaria.repositories.ConcessionarioRepository;
 
 @Controller
 public class ControllerAuto {
 
     private final AutoRepository autoRepository;
+    private final ConcessionarioRepository concessionarioRepository;
 
-    public ControllerAuto(AutoRepository autoRepository) {
+    public ControllerAuto(AutoRepository autoRepository, ConcessionarioRepository concessionarioRepository) {
         this.autoRepository = autoRepository;
+        this.concessionarioRepository = concessionarioRepository;
     }
 
     // @GetMapping("/automobili")
@@ -48,6 +54,19 @@ public class ControllerAuto {
         }
         model.addAttribute("listaAutomobili", listaAutoFiltrate);
         return "listaauto";
+    }
+
+    @GetMapping("/addauto")
+    public String getForm(Model model) {
+        model.addAttribute("concessionarie", concessionarioRepository.findAll());
+        return "formauto";
+    }
+
+    @PostMapping("/addauto")
+    // @ResponseBody
+    public String addAuto(Auto auto) {
+        autoRepository.save(auto);
+        return "redirect:/listaauto";
     }
     
 }
